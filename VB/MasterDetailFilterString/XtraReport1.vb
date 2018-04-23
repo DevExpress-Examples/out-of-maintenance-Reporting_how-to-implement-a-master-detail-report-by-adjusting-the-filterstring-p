d77be2301b9d2d25e73af770717fa3900b0e8dac
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/MasterDetailFilterString/XtraReport1.vb
@@ -0,0 +1,21 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Drawing
+Imports System.Collections
+Imports System.ComponentModel
+Imports DevExpress.XtraReports.UI
+
+Namespace MasterDetailFilterString
+	Partial Public Class XtraReport1
+		Inherits DevExpress.XtraReports.UI.XtraReport
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub DetailReport_BeforePrint(ByVal sender As Object, ByVal e As System.Drawing.Printing.PrintEventArgs) Handles DetailReport.BeforePrint
+			Dim orderId As Integer = Convert.ToInt32(Me.GetCurrentColumnValue("OrderID"))
+			CType(sender, DetailReportBand).FilterString = String.Format("[OrderID] = {0}", orderId)
+		End Sub
+
+	End Class
+End Namespace
